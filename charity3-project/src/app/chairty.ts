bf7ef8399aaa5299a1032bd621962eb367d667fd
Courtney Tyler <courtneytyler7@gmail.com>
2019-07-04 15:14:47 -0400

    add charities component, add charity type and seed data files

---
+++ charity3-project/src/app/chairty.ts
@@ -0,0 +1,7 @@
+export class Charity {
+  name: string;
+  location: string;
+  cause: string;
+  site: string;
+  charityNavSite: string;
+}
---
+++ charity3-project/src/app/charity-data.ts
@@ -0,0 +1,31 @@
+import { Charity } from "./chairty";
+
+export const CHARITY: Charity[] = [
+  {
+    name: "SOME (So Others Might Eat)",
+    location: "Washington, DC",
+    cause:
+      "To help the poor and homeless of our nation's capital. To meet the immediate daily needs of the people we serve with food, clothing, and health care. To help break the cycle of homelessness by offering services, such as affordable housing, job training, addiction treatment, and counseling, to the poor, the elderly and individuals with mental illness",
+    site: "https://www.some.org/",
+    charityNavSite:
+      "https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=6077"
+  },
+  {
+    name: "ALSAC - St. Jude Children's Research Hospital",
+    location: "Memphis, TN",
+    cause:
+      "To raise funds to support the operating and maintenance of St. Jude Children's Research Hospital. The mission of St. Jude Children's Research Hospital is to find cures for children with cancer and other catastrophic diseases through research and treatment. It is supported primarily by donations raised by ALSAC.",
+    site: "https://www.stjude.org/",
+    charityNavSite:
+      "https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=12847"
+  },
+  {
+    name: "ACLU - American Civil Liberties Union Foundation",
+    location: "New York, NY",
+    cause:
+      "To provide legal presentation in cases involving issues of civil liberties and constitutional rights ranging from administrative hearings to trials and appeals. In addition, the Foundation educates the public as to civil liberties and constitutional rights by disseminating literature and other publications. The ACLU Foundation preserves and promotes civil rights and liberties as guaranteed by the United States Constitution.",
+    site: "https://www.aclu.org/",
+    charityNavSite:
+      "https://www.charitynavigator.org/index.cfm?bay=search.summary&orgid=3247"
+  }
+];
